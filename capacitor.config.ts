5bb385e375954646b3b0ad51cf9a0748a16687a6
Madhan <madhanmohanreddy788@gmail.com>
2025-12-31 17:21:41 +0530

    Initial commit - React Vite with Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gpconnect.com',
+  appName: 'PGCONNECT',
+  webDir: 'dist'
+};
+
+export default config;
